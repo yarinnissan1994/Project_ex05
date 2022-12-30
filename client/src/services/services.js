ca604bb1da460d86fc9b17d3ab6e4667e9f010a2
yarinnissan1994 <yarinnissan1994@gmail.com>
2022-12-30 04:15:44 +0200

    added update and delete

--- client/src/services/services.js
+++ client/src/services/services.js
@@ -8,3 +8,11 @@ export const getProducts = async () => {
 export const addCostumerMessage = async (CostumerMessage) => {
   await axios.post("http://localhost:7147/api/Products", CostumerMessage);
 };
+
+// export const getProducts = async () => {
+//   return await axios
+//     .get("http://localhost:7147/api/Products")
+//     .then((response) => {
+//       return Object.values(response.data);
+//     });
+// };
